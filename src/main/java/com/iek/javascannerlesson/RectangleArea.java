be97abbfdad82099cd61d09241d9c78f9ce0889d
Spyros <Spyros@Markos.mshome.net>
2022-10-23 11:36:22 +0300

    scanner initial commit

---
+++ src/main/java/com/iek/javascannerlesson/RectangleArea.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.iek.javascannerlesson;
+
+/**
+ *
+ * @author Spyros
+ */
+import java.util.Scanner;    // Needed for Scanner class
+
+/**
+ *  This program demonstrates keyboard input.
+ */
+public class RectangleArea
+{
+   public static void main(String[] args)
+   {
+      int length;    // To hold rectangle's length.
+      int width;     // To hold rectangle's width.
+      int area;      // To hold rectangle's area
+
+      // Create a Scanner object to read input.
+      Scanner console = new Scanner(System.in);
+
+      // Get length from the user.
+      System.out.print("Enter length ");
+      length = console.nextInt();
+
+      // Get width from the user.
+      System.out.print("Enter width ");
+      width = console.nextInt();
+
+      // Calculate area.
+      area = length * width;
+
+      // Display area.
+      System.out.println("The area of rectangle is " + area);
+   }
+}
